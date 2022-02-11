ff53fe8b9d3792afb782b580cf8aca9792dac14c
bhuvan-b <bokkabhuvan2@gmail.com>
2022-02-12 01:41:02 +0530

    Added Hospital Signup and Icon

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import { Route, Routes } from "react-router-dom";
 import Album from "./components/Album";
 import SignIn from "./components/Login";
 import SignUp from "./components/Signup";
+import HospSignup from "./components/hospital/HospSignup";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Album />} />
-            <Route path="/login" exact element={<SignIn />} />
-            <Route path="/signup" exact element={<SignUp />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Album />} />
+      <Route path="/login" exact element={<SignIn />} />
+      <Route path="/signup" exact element={<SignUp />} />
+      <Route path="/hospsignup" exact element={<HospSignup />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/components/hospital/HospSignup.jsx
@@ -0,0 +1,165 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { FormControl, FormControlLabel, FormLabel } from "@mui/material";
+import { Radio, RadioGroup } from "@mui/material";
+
+const theme = createTheme();
+
+export default function HospSignup() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Hospital Sign up
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  autoComplete="given-name"
+                  name="hospitalName"
+                  required
+                  fullWidth
+                  id="hospitalName"
+                  label="Hospital Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Hospital Registration ID"
+                  name="HospitalId"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="ContactNumber"
+                  label="Contact Number"
+                  name="ContactNumber"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="address"
+                  label="Hospital Address"
+                  name="HospitalAddress"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl required>
+                  <FormLabel id="ownership">Ownership</FormLabel>
+                  <RadioGroup
+                    required
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="ownership-radio-buttons-group"
+                  >
+                    <FormControlLabel
+                      value="public"
+                      control={<Radio />}
+                      label="Public"
+                    />
+                    <FormControlLabel
+                      value="private"
+                      control={<Radio />}
+                      label="Private"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              {/* <Grid item xs={12}></Grid> */}
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Typography component={Link} to="/login" variant="body2">
+                  Already have an account? Sign in
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Typography component={Link} to="/" variant="body2">
+                  Back to Home
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        {/* <Copyright sx={{ mt: 5 }} /> */}
+      </Container>
+    </ThemeProvider>
+  );
+}
